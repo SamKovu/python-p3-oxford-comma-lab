2b9b4f33623912696686f04e66a7b6e567715477
Samuel Gichuhi <gichuhi1sam@gmail.com>
2023-12-05 02:01:40 +0300

    Completed lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+    
+    if len(items)==2:
+           return " and ".join(items) 
+    elif len(items)>2:
+         lastItem=items.pop()
+         sentence=", ".join(items)
+         return sentence+", and "+ lastItem
+    else:
+         return "".join(items)
+
+   
